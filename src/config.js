d882a4e873604acaa6195a6afb4f30eb75ae6b2c
tech927 <onnui688@gmail.com>
2025-10-23 14:43:45 -0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const CONFIG = {
+  SITE_NAME: "INCONNU BOY SENSEI",
+  TELEGRAM: "@just_me_inconnu",
+  GMAIL: "inconnuboytech@gmail.com",
+  MINI_BOT_URL: "https://mini-bot-dnu2.onrender.com",
+  YOUTUBE_CHANNEL: "https://www.youtube.com/@inconnuboy-b5h",
+  MONGODB_URI: "mongodb+srv://onnui688:FSybn9yxLAYpGrZj@purge-quizz.aejj5y8.mongodb.net/?retryWrites=true&w=majority&appName=Purge-quizz"
+};
